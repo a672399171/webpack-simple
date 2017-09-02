502202427cadcdd9a12074cd5128c9e396fb2a3c
Administrator <672399171@qq.com>
2017-09-02 11:10:35 +0800

    去掉bootstrap按钮边框

--- src/router/index.js
+++ src/router/index.js
@@ -8,28 +8,46 @@ import Full from 'containers/Full'
 import Dashboard from 'views/Dashboard'
 import NotFoundComponent from 'views/Page404'
 
+// Views - Components
+import Buttons from 'views/components/Buttons'
+
+import App from 'src/App'
+
 Vue.use(Router);
 
 export default new Router({
-	mode: 'hash', // todo
-	linkActiveClass: 'open active',
-	routes: [
-		{
-			path: '/',
-			redirect: '/dashboard',
-			name: 'Home',
-			component: Full,
-			children: [
-				{
-					path: 'dashboard',
-					name: 'Dashboard',
-					component: Dashboard
-				}
-			]
-		},
-		{
-			path: '*',
-			component: NotFoundComponent
-		}
-	]
+    mode: 'hash', // todo
+    linkActiveClass: 'open active',
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard',
+            name: 'Home',
+            component: Full,
+            children: [
+                {
+                    path: 'dashboard',
+                    name: 'Dashboard',
+                    component: Dashboard
+                },
+                {
+                    path: 'components',
+                    name: 'Components',
+                    redirect: '/components/buttons',
+                    component: App,
+                    children: [
+                        {
+                            path: 'buttons',
+                            name: 'Buttons',
+                            component: Buttons
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            path: '*',
+            component: NotFoundComponent
+        }
+    ]
 })
